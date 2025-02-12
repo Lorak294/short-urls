6e9543f49719dee160c8122b63f8fb566cfb1fc3
Karol Kącki <karol.kacki2@gmail.com>
2025-02-12 23:52:08 +0100

    redis db implementation

--- api/database/IDatabaseClient.go
+++ api/database/IDatabaseClient.go
@@ -5,14 +5,15 @@ import "time"
 type DatabaseClient interface {
 
 	// state handling
-	Initialize() error
-	Close() error
+	Initialize() DatabaseClient
+	Close()
 
 	// short urls
-	ResolveShortUrl(url string) string
+	ResolveShortUrl(url string) (string, error)
 
 	// rate limiting
 	GetRateLimitForIp(ip string) (int, error)
 	SetRateLimitForIp(ip string, limit int, ttl time.Duration) error
 	DecrementRateLimitForIp(ip string) (error)
+	GetLeftRateLimitTime(ip string) (time.Duration, error)
 }
--- api/database/db_redis/RedisDbClient.go
+++ api/database/db_redis/RedisDbClient.go
@@ -2,7 +2,11 @@ package db_redis
 
 import (
 	"context"
+	"os"
+	"strconv"
 	"time"
+
+	"github.com/redis/go-redis/v9"
 )
 
 const SHORT_URLS_DB_NR = 0
@@ -10,32 +14,53 @@ const COUNTER_DB_NR = 1
 var CTX = context.Background()
 
 type RedisDbClient struct {
-
+	shortsDbClient *redis.Client
+	rateLimitDbClient *redis.Client
 }
 
 // state handling
-func (x RedisDbClient)Initialize() error{
-
+func (x RedisDbClient)Initialize() RedisDbClient {
+	x.shortsDbClient = redis.NewClient(&redis.Options{
+		Addr: os.Getenv("DB_ADDR"),
+		Password: os.Getenv("DB_PASS"),
+		DB: SHORT_URLS_DB_NR,
+	})
+	x.rateLimitDbClient = redis.NewClient(&redis.Options{
+		Addr: os.Getenv("DB_ADDR"),
+		Password: os.Getenv("DB_PASS"),
+		DB: SHORT_URLS_DB_NR,
+	})
+	return x
 }
 
-func (x RedisDbClient)Close() error {
-
+func (x RedisDbClient)Close() {
+	x.shortsDbClient.Close()
+	x.rateLimitDbClient.Close()
 }
 
 // short urls
-func (x RedisDbClient)ResolveShortUrl(url string) string {
-
+func (x RedisDbClient)ResolveShortUrl(url string) (string, error) {
+	return x.shortsDbClient.Get(CTX,url).Result()
 }
 
 // rate limiting
 func (x RedisDbClient)GetRateLimitForIp(ip string) (int, error) {
+	res_str, err := x.rateLimitDbClient.Get(CTX,ip).Result()
+	if  err != nil {
+		return -1, err
+	}
+	return strconv.Atoi(res_str)
+}
 
+func (x RedisDbClient)GetLeftRateLimitTime(ip string) (time.Duration, error) {
+	res := x.rateLimitDbClient.TTL(CTX,ip)
+	return res.Result()
 }
 
 func (x RedisDbClient)SetRateLimitForIp(ip string, limit int, ttl time.Duration) error{
-
+	return x.rateLimitDbClient.Set(CTX,ip,limit,ttl).Err()
 }
 
 func (x RedisDbClient)DecrementRateLimitForIp(ip string) (error) {
-
+	return x.rateLimitDbClient.Decr(CTX,ip).Err()
 }
